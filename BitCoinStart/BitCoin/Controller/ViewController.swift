70ef859c74ebac8b152ca5e968a698e21ad92cfa
Thomas Månsson <ThomasCreate@thomascreate.mynet>
2022-05-06 10:51:26 +0200

    First commit starter projekt

---
+++ BitCoinStart/BitCoin/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitCoin
+//
+//  Created by Thomas Månsson on 11/02/2022.
+//  Copyright © 2022 ThomasCreate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ BitCoinStart/BitCoin/Model/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  BitCoin
+//
+//  Created by Thomas Månsson on 11/02/2022.
+//  Copyright © 2022 ThomasCreate. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    
+}
