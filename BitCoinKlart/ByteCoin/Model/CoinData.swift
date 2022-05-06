6bb275f8a7655e30aaa9130acd3032ffd2732c6d
Thomas Månsson <ThomasCreate@thomascreate.mynet>
2022-05-06 11:01:48 +0200

    Second

---
+++ BitCoinKlart/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  BitCoin
+//
+//  Created by Thomas Månsson on 11/02/2022.
+//  Copyright © 2022 ThomasCreate. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
